98f8e104586103b294eac76be8b3eb44819916b4
Preston Elliott <preston.elliott24@gmail.com>
2019-07-18 11:25:50 -0400

    authorization working in postman and sessions setup

--- trail-explorer-backend/app/models/user.rb
+++ trail-explorer-backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :follows
     has_many :followed_users, through: :follows
   
--- trail-explorer-backend/db/schema.rb
+++ trail-explorer-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_194959) do
+ActiveRecord::Schema.define(version: 2019_07_18_145706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2019_07_17_194959) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
