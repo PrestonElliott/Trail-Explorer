e9b356d23a28f31034a8caf48bc152ce12303042
Preston Elliott <preston.elliott24@gmail.com>
2019-07-29 15:24:29 -0400

    post new trips with trails to backend

--- trail-explorer-backend/app/controllers/trails_controller.rb
+++ trail-explorer-backend/app/controllers/trails_controller.rb
@@ -1,4 +1,5 @@
 class TrailsController < ApplicationController
+    skip_before_action :authorized, only: [:index]
     
     def show
         begin @trail = Trail.find(params[:id])
--- trail-explorer-frontend/src/App.js
+++ trail-explorer-frontend/src/App.js
@@ -5,10 +5,10 @@ import { connect } from 'react-redux'
 import Navbar from "./Components/Navbar"
 import Login from "./Components/Login"
 import Profile from "./Components/Profile"
-import EditProfile from './Components/EditProfile';
+import EditProfile from './Components/EditProfile'
 import FollowerFeed from "./Components/FollowerFeed"
 import SignUp from "./Components/SignUp"
-import TrailsHome from './Components/TrailsHome';
+import TrailsHome from './Components/TrailsHome'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "shards-ui/dist/css/shards.min.css"
