5131a0afa11e8ea3702b4928e229d40d6d89f492
Preston Elliott <preston.elliott24@gmail.com>
2019-07-29 14:01:57 -0400

    worked on trips and future trips

--- trail-explorer-frontend/src/Components/FollowerFeed.jsx
+++ trail-explorer-frontend/src/Components/FollowerFeed.jsx
@@ -1,20 +1,33 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import { connect } from 'react-redux'
 import Trip from './Trip'
+import FutureTrip from "./FutureTrip"
 
 class FollowerFeed extends Component {
 
     render() {
         return (
-            <div className='tripcard-div'>
-            {
-                this.props.user.followed_users.map(friend => {
-                    return friend.trips.map(trip => {
-                        return <Trip key={trip.id} trip={trip} />
-                    })
-                })
-            }
-        </div>
+            <Fragment>
+                <div className='tripcard-div'>
+                    {
+                        this.props.user.followed_users.map(friend => {
+                            return friend.trips.map(trip => {
+                                return <Trip key={trip.id} trip={trip} />
+                            })
+                        })
+                    }
+                </div>
+
+                <div className='futuretripcard-div'>
+                    {
+                        this.props.user.followed_users.map(friend => {
+                            return friend.futureTrips.map(futureTrip => {
+                                return <FutureTrip key={futureTrip.id} futureTrip={futureTrip} />
+                            })
+                        })
+                    }
+                </div>
+            </Fragment>
         )
     }
 }
